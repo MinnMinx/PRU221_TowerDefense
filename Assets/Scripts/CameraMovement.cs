653c284e712527e2d46fabc8285babd216ca073c
DESKTOP-BI6M1B7\DELL <theultimatrix2@gmail.com>
2023-06-01 03:27:27 +0700

    Add TMPro and add camera scrolling movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 minMaxOffsetX;
+    [SerializeField]
+    private Vector2 minMaxOffsetY;
+    [SerializeField]
+    private float moveSpeed = 5f;
+    private Camera mainCam;
+    private Vector3 originalPos, centerScreenPos;
+    private static Vector2 CENTER_VIEWPORT_POS = new Vector2(0.5f, 0.5f);
+    private Vector2 mouseStartPos;
+    private bool _allowScroll = false;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        originalPos = transform.position;
+        _allowScroll = false;
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetScrolling(!_allowScroll);
+        }
+#endif
+
+        if (_allowScroll)
+        {
+            Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 distance = mouseWorldPos - centerScreenPos;
+            distance = new Vector3
+            {
+                x = Mathf.Clamp(distance.x, minMaxOffsetX.x, minMaxOffsetX.y),
+                y = Mathf.Clamp(distance.y, minMaxOffsetY.x, minMaxOffsetY.y),
+                z = 0,
+            };
+            transform.position = Vector3.Lerp(transform.position, originalPos + distance, Time.deltaTime * moveSpeed);
+        }
+
+    }
+
+    public void SetScrolling(bool scrolling = true)
+    {
+        _allowScroll = scrolling;
+        if (_allowScroll)
+        {
+            mouseStartPos = mainCam.ScreenToWorldPoint(mouseStartPos);
+            centerScreenPos = mainCam.ViewportToWorldPoint(CENTER_VIEWPORT_POS);
+        }
+    }
+}
