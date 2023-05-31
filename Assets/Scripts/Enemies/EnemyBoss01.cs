b0e5eb29c970091c0df32e6e6df92329103a9990
Anh Pham <phamanhqb73@gmail.com>
2023-05-31 07:24:25 +0700

    thêm script boss

---
+++ Assets/Scripts/Enemies/EnemyBoss01.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoss01 : Enemy01_Base
+{
+    public EnemyBoss01(decimal hp, decimal atk, float speed, decimal money) : base(hp, atk, speed, money)
+    {
+    }
+
+    public override bool OnUpdate()
+    {
+        if(Hp < Hp / 2)
+        {
+            Speed += 3.5f;
+        }
+        return base.OnUpdate();
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyBoss03.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoss03 : Enemy01_Base
+{
+    public EnemyBoss03(decimal hp, decimal atk, float speed, decimal money) : base(hp, atk, speed, money)
+    {
+    }
+
+    public override bool OnUpdate()
+    {
+        if (Hp >= Hp / 2)
+        {
+            Speed += 3.5f;
+        }
+        else
+        {
+            Atk += 40;
+
+            // miễn sát thương 3s
+        }
+        return base.OnUpdate();
+    }
+}
